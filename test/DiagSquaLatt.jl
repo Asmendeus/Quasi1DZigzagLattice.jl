da269b0140931901a6bc3f9b86a7d9d9399b5e6d
Asmendeus <1422805255@qq.com>
2025-07-20 22:45:57 +0800

    up test cases

--- test/DiagSquaLatt.jl
+++ test/DiagSquaLatt.jl
@@ -1,11 +1,13 @@
-latt = DiagTriaLatt(4, 4)
+latt = DiagSquaLatt(4, 4)
 
 @show latt
 
 @show getAllSites(latt)
 
-@show getAllNNPairs(latt; boundary=:OBC)
 @show getAllNNPairs(latt; boundary=:PBC)
+@assert all(x->x in getAllNNPairs(latt; boundary=:PBC), getAllNNPairs(latt; boundary=:OBC))
+@show setdiff(getAllNNPairs(latt; boundary=:PBC), getAllNNPairs(latt; boundary=:OBC))
 
 @show getAllNNNPairs(latt; boundary=:OBC)
-@show getAllNNNPairs(latt; boundary=:PBC)
+@assert all(x->x in getAllNNNPairs(latt; boundary=:PBC), getAllNNNPairs(latt; boundary=:OBC))
+@show setdiff(getAllNNNPairs(latt; boundary=:PBC), getAllNNNPairs(latt; boundary=:OBC))
--- test/SquaLatt.jl
+++ test/SquaLatt.jl
@@ -4,8 +4,10 @@ latt = SquaLatt(4, 4)
 
 @show getAllSites(latt)
 
-@show getAllNNPairs(latt; boundary=:OBC)
 @show getAllNNPairs(latt; boundary=:PBC)
+@assert all(x->x in getAllNNPairs(latt; boundary=:PBC), getAllNNPairs(latt; boundary=:OBC))
+@show setdiff(getAllNNPairs(latt; boundary=:PBC), getAllNNPairs(latt; boundary=:OBC))
 
 @show getAllNNNPairs(latt; boundary=:OBC)
-@show getAllNNNPairs(latt; boundary=:PBC)
+@assert all(x->x in getAllNNNPairs(latt; boundary=:PBC), getAllNNNPairs(latt; boundary=:OBC))
+@show setdiff(getAllNNNPairs(latt; boundary=:PBC), getAllNNNPairs(latt; boundary=:OBC))
